a377526b6dd157ba7e0cff25ab971587422584a8
MUBIRI <mubirirobberts@gmail.com>
2022-06-02 04:35:41 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main; print the string that prints the put function
+ *
+ * description; using the main function
+ * this prints "Programming is like building a multilingual puzzle"
+ * return:(0)
+ */
+int main(void)
+{
+          puts("\"Programming is like building a multilingual puzzle")
+	  return(0)
+}
