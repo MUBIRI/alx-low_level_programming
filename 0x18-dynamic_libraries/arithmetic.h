ab4e52d8a17383057f15e074c5fa841fd329da87
MUBIRI <mubirirobberts@gmail.com>
2022-09-07 02:00:59 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/arithmetic.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+int main();
+int getSum(int num1, int num2);
+int getDifference(int num1, int num2);
+int getProduct(int num1, int num2);
+float getQuotient(int num1, int num2);
+int getModulo(int num1, int num2);
+#endif
